d466dc4c3a2a72ac39b083aa44046b5276de398a
james-yf <jamezyossef7@gmail.com>
2025-07-31 15:25:03 +1000

    made it cleaner

--- Task6.2C/client.py
+++ Task6.2C/client.py
@@ -1,7 +1,7 @@
 import socket
 
-server_addr = 'localhost'
-server_port = 11124
+"""tuple containing IP and Port#"""
+server_info = ('localhost', 11124)
 response_buff = 100
 send_query = 'y'
 
@@ -13,13 +13,14 @@ try:
             break
         
         domain_name = input("Enter a domain name: ")
-        client_socket.sendto(domain_name.encode(), (server_addr, server_port))
-        server_response, server_info = client_socket.recvfrom(response_buff)
+        client_socket.sendto(domain_name.encode(), server_info)
+        server_response, _ = client_socket.recvfrom(response_buff)
         print(f"The server responded with: {server_response.decode()}")
 
         send_query = input("\nDo you want to send another DNS query? (y/n): ")
+
 finally:
     print("The client program is terminating...")
     # tell the server program to terminate
-    client_socket.sendto(("terminate").encode(), (server_addr, server_port))
+    client_socket.sendto(("terminate").encode(), server_info)
     client_socket.close()
