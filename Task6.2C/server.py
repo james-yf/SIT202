a8712fee23d0986edd6fe2fd4e2055f0056a5699
james-yf <jamezyossef7@gmail.com>
2025-07-31 15:30:53 +1000

    cleaned it up

--- Task6.2C/server.py
+++ Task6.2C/server.py
@@ -1,6 +1,6 @@
 import socket
 
-# (alias name : canonical name)
+# alias name : canonical name
 CNAME_records = {
     "blog.pamelabeesly.com": "pamelabeesly.com",
     "www.pamelabeesly.com": "pamelabeesly.com",
@@ -10,25 +10,21 @@ CNAME_records = {
     "www.michaelscott.com": "michaelscott.com"
 }
 
-# (canonical name : IP address)
+# canonical name : IP address
 A_records = {
     "pamelabeesly.com": "45.67.89.123",
     "jimhalpert.com": "102.154.76.88",
     "michaelscott.com": "172.32.210.5"
 }
 
-server_addr = 'localhost'
-server_port = 11124
+server_info = ('localhost', 11124)
 request_buff = 100
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-server_socket.bind((server_addr, server_port))
+server_socket.bind(server_info)
 
 while True:
-    # client_info is a tuple: (IP address, Port number)
     encoded_domain_name, client_info = server_socket.recvfrom(request_buff)
-
     domain_name = encoded_domain_name.decode()
 
     if domain_name in A_records:
