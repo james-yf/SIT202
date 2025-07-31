5b49b8c186007fc0abe935909dfd2102d4f58df3
james-yf <jamezyossef7@gmail.com>
2025-07-31 15:50:12 +1000

    cleaned again

--- Task6.2C/server.py
+++ Task6.2C/server.py
@@ -1,5 +1,12 @@
 import socket
 
+# canonical name : IP address
+A_records = {
+    "pamelabeesly.com": "45.67.89.123",
+    "jimhalpert.com": "102.154.76.88",
+    "michaelscott.com": "172.32.210.5"
+}
+
 # alias name : canonical name
 CNAME_records = {
     "blog.pamelabeesly.com": "pamelabeesly.com",
@@ -10,29 +17,22 @@ CNAME_records = {
     "www.michaelscott.com": "michaelscott.com"
 }
 
-# canonical name : IP address
-A_records = {
-    "pamelabeesly.com": "45.67.89.123",
-    "jimhalpert.com": "102.154.76.88",
-    "michaelscott.com": "172.32.210.5"
-}
-
 server_info = ('localhost', 11124)
-request_buff = 100
+query_buff = 100
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(server_info)
 
 while True:
-    encoded_domain_name, client_info = server_socket.recvfrom(request_buff)
-    domain_name = encoded_domain_name.decode()
+    query_message, client_info = server_socket.recvfrom(query_buff)
+    domain_name = query_message.decode()
 
     if domain_name in A_records:
-        domain_addr = A_records[domain_name]
-        server_socket.sendto(domain_addr.encode(), client_info)
+        domain_IP = A_records[domain_name]
+        server_socket.sendto(domain_IP.encode(), client_info)
     elif domain_name in CNAME_records:
-        domain_addr = A_records[CNAME_records[domain_name]]
-        server_socket.sendto(domain_addr.encode(), client_info)
+        domain_IP = A_records[CNAME_records[domain_name]]
+        server_socket.sendto(domain_IP.encode(), client_info)
     elif domain_name == "terminate":
         print("The server program is terminating...")
         break
