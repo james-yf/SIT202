70a652465a38740f5b63ba4817387e4273f34f66
james-yf <jamezyossef7@gmail.com>
2025-07-31 22:18:14 +1000

    pseudocode for 6.2C

---
+++ Task5.2C.py
@@ -0,0 +1,41 @@
+"""
+initialise dictionary CNAME_records
+initialise dictionary A_records.
+
+initalise server_addr = localhost 
+initalise server_port = some port #
+
+create server socket
+
+bind the socket to server_addr and server_port
+
+print "server is waiting for a query"
+
+loop infinitely
+    receive query from the client
+
+    if query == quit
+        exit loop
+
+    elif query is in A_records
+        get the IP address from A_records
+        send the IP address to the client
+
+    elif query is in CNAME_records
+        if CNAME is in A_records
+            get the IP address from A_records
+            send it to the client
+        else
+            send "the domain does not exist in A records"
+
+    else
+        send "the domain does not exist" to the client
+
+close the socket
+
+"""
+
+
+
+
+
