f397c5ea34e4b8d74dc3adf8fad1fb48e67207f5
james-yf <jamezyossef7@gmail.com>
2025-08-05 22:57:56 +1000

    server-side code

---
+++ Mod3_ActiveClass4_Activity3/server.py
@@ -0,0 +1,26 @@
+import socket
+
+server_info = ('localhost', 11124)
+query_buff = 100
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(server_info)
+
+print("The server is running!")
+
+while True:
+    _, client_info = server_socket.recvfrom(query_buff)
+    message = _.decode()
+    
+    if message == "terminate":
+        print("The server program is terminating...")
+        break
+
+    char_count = len(message)
+    uppercase_message = message.upper()
+    response_message = str(char_count) + " " + uppercase_message
+
+    print(f"{char_count}")
+    server_socket.sendto(response_message.encode(), client_info)
+    
+server_socket.close()
