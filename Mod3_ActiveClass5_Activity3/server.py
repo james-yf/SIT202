eb63b6875a78cfec77e71bf77b35c8bb26866c2e
james-yf <jamezyossef7@gmail.com>
2025-08-09 22:25:29 +1000

    moved start-up message

--- Mod3_ActiveClass5_Activity3/server.py
+++ Mod3_ActiveClass5_Activity3/server.py
@@ -5,9 +5,8 @@ server_socket.bind(client_info)
 
 message_buff = 100
 server_socket.listen(1)
-connectCl1Socket, _ = server_socket.accept()
-
 print("The server is running!")
+connectCl1Socket, _ = server_socket.accept()
 
 while True:
     message = connectCl1Socket.recv(message_buff)
